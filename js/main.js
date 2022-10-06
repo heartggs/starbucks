f0b2d1b4c2b384f845e1889243354898c488e88e
heartggs <jounghj0806@naver.com>
2022-10-06 22:42:29 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-const searchEL = document.querySelector('.search');
-const searchInputEL = searchEL.querySelector('input');
-
-searchEL.addEventListener('click', function(){
-  searchInputEL.focus();
-});
-
-searchInputEL.addEventListener('focus',function(){
-  searchEL.classList.add('focused');
-  searchInputEL.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEL.addEventListener('blur',function(){
-  searchEL.classList.remove('focused');
-  searchInputEL.setAttribute('placeholder', '');
-});
-
-
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -149,6 +130,3 @@ spyEls.forEach(function (spyEl) {
   .setClassToggle(spyEl, 'show')
   .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //올해의 년도로 나옴
